c8aff202f2fb4c644512733487f6f81fc427b77f
Corentin GASPART <cgaspart@e1r11p23.42.fr>
2018-04-05 15:57:57 +0200

    0 Leaks | -lartR OK

--- src/ft_nf_arg_ascii.c
+++ src/ft_nf_arg_ascii.c
@@ -6,11 +6,43 @@
 /*   By: cgaspart <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/07 21:27:37 by cgaspart          #+#    #+#             */
-/*   Updated: 2018/02/07 21:27:39 by cgaspart         ###   ########.fr       */
+/*   Updated: 2018/04/05 15:54:33 by cgaspart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
+
+static int	get_nbr_arg_dir(int argc, char **argv)
+{
+	int i;
+	int j;
+
+	i = ft_skip_flag(argc, argv);
+	j = 0;
+	while (i < argc)
+	{
+		if (!ft_is_file(argv[i]) && ft_error_np(argv[i]))
+			j++;
+		i++;
+	}
+	return (j);
+}
+
+static int	get_nbr_arg_file(int argc, char **argv)
+{
+	int i;
+	int j;
+
+	i = ft_skip_flag(argc, argv);
+	j = 0;
+	while (i < argc)
+	{
+		if (ft_is_file(argv[i]) && ft_error_np(argv[i]))
+			j++;
+		i++;
+	}
+	return (j);
+}
 
 char		**ft_nf_arg_ascii_file(int argc, char **argv)
 {
@@ -20,12 +52,12 @@ char		**ft_nf_arg_ascii_file(int argc, char **argv)
 
 	i = 0;
 	j = 1;
-	res = malloc(sizeof(char*) * argc);
+	res = malloc(sizeof(char*) * get_nbr_arg_file(argc, argv) + 1);
 	while (j < argc)
 	{
 		if (ft_is_file(argv[j]) && ft_error(argv[j], 0))
 		{
-			res[i] = argv[j];
+			res[i] = ft_strdup(argv[j]);
 			i++;
 		}
 		j++;
@@ -44,17 +76,18 @@ char		**ft_nf_arg_ascii_dir(int argc, char **argv)
 
 	i = 0;
 	j = 1;
-	res = malloc(sizeof(char*) * argc);
+	res = malloc(sizeof(char*) * get_nbr_arg_dir(argc, argv) + 1);
 	while (j < argc)
 	{
 		if (!ft_is_file(argv[j]) && ft_error(argv[j], 0))
 		{
-			res[i] = argv[j];
+			res[i] = ft_strdup(argv[j]);
 			i++;
 		}
 		j++;
 	}
 	res[i] = NULL;
-	res = ft_tabascii(res);
+	if (res[0] != NULL)
+		res = ft_tabascii(res);
 	return (res);
 }
--- src/ft_nf_multi.c
+++ src/ft_nf_multi.c
@@ -6,35 +6,41 @@
 /*   By: cgaspart <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/08 00:22:33 by cgaspart          #+#    #+#             */
-/*   Updated: 2018/02/08 00:33:35 by cgaspart         ###   ########.fr       */
+/*   Updated: 2018/04/05 15:52:55 by cgaspart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-void	ft_nf_multi(int argc, char **argv)
+static void		nf_dir(char **argfile, char **argdir)
+{
+	int i;
+
+	i = 0;
+	if (argfile[0])
+		ft_putstr("\n");
+	while (argdir[i])
+	{
+		ft_putstr(argdir[i]);
+		ft_putstr(":\n");
+		ft_simple(argdir[i]);
+		if (argdir[i + 1] != NULL)
+			ft_putstr("\n");
+		i++;
+	}
+}
+
+void			ft_nf_multi(int argc, char **argv)
 {
 	char	**argfile;
 	char	**argdir;
-	int		i;
 
-	i = 0;
 	argdir = ft_nf_arg_ascii_dir(argc, argv);
 	argfile = ft_nf_arg_ascii_file(argc, argv);
 	if (argfile[0])
 		ft_puttab(argfile);
 	if (argdir[0])
-	{
-		if (argfile[0])
-			ft_putstr("\n");
-		while (argdir[i])
-		{
-			ft_putstr(argdir[i]);
-			ft_putstr(":\n");
-			ft_simple(argdir[i]);
-			if (argdir[i + 1] != NULL)
-				ft_putstr("\n");
-			i++;
-		}
-	}
+		nf_dir(argfile, argdir);
+	ft_free_tab(argfile);
+	ft_free_tab(argdir);
 }
